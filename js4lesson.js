f167986198e9fa30822f176509c0339c820a35c3
AlQalb <hayotbekyoldashev2@gmail.com>
2021-07-09 23:40:32 +0500

    grade

---
+++ js4lesson.js
@@ -0,0 +1,41 @@
+grade = 78
+
+if(grade>0 && grade<=59 ) {
+    console.log(`F`);
+}
+else if  (grade>=60 && grade<=62 ){
+    console.log(`D-`);
+}
+else if  (grade>=63 && grade<=66 ){
+    console.log(`D`);
+}
+else if  (grade>=67 && grade<=69 ){
+    console.log(`D+`);
+}
+else if  (grade>=70 && grade<=72 ){
+    console.log(`C-`);
+}
+else if  (grade>=73 && grade<=76 ){
+    console.log(`C`);
+}
+else if  (grade>=77 && grade<=79 ){
+    console.log(`C+`);
+}
+else if  (grade>=80 && grade<=82 ){
+    console.log(`B-`);
+}
+else if  (grade>=83 && grade<=86 ){
+    console.log(`B`);
+}
+else if  (grade>=87 && grade<=89 ){
+    console.log(`B+`);
+}
+else if  (grade>=90 && grade<=92 ){
+    console.log(`A-`);
+}
+else if  (grade>=93 && grade<=96 ){
+    console.log(`A`);
+}
+else if  (grade>=97 && grade<=100 ){
+    console.log(`A+`);
+}
