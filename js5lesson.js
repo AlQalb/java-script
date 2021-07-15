3301569843a610c421bf1c4376f963872a2e446c
AlQalb <hayotbekyoldashev2@gmail.com>
2021-07-15 12:10:19 +0500

    for loop

---
+++ js5lesson.js
@@ -0,0 +1,19 @@
+let i=3
+for(i; i<=10; i++){
+      if(i%2==0 ){ console.log(`${i} juftson`);}
+     else {console.log(`${i} toq son`);}
+}
+
+let b=10
+for(b; b>=1; b=b-1){
+      if(b%2==0 ){ console.log(`${b} juftson`);}
+     else {console.log(`${b} toq son`);}
+}
+
+let k=1
+for(k; k<=100; k++){
+    if (k%5==0&&k%3==0){console.log(`${k} 5 ga va 3 ga`);}
+else if (k%3==0){console.log(`${k} 3 ga`);}
+else if (k%5==0){console.log(`${k} 5 ga`);}
+
+}
